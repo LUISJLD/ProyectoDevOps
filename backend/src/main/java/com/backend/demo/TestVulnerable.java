cfe38e49246de0bffe07ca86ed91724001d2d17f
Jeferson Florez <jeffersonflorez73@gmail.com>
2026-05-04 20:18:03 -0500

    test: add detectable vulnerability for semgrep validation

--- backend/src/main/java/com/backend/demo/TestVulnerable.java
+++ backend/src/main/java/com/backend/demo/TestVulnerable.java
@@ -1,7 +1,7 @@
 package com.backend.demo;
 
 public class TestVulnerable {
-
+    public String PASSWORD="admin123";
     public void run(String input) throws Exception {
         Runtime.getRuntime().exec(input);
     }
